d387999c176a44d1c8aafee40861642fdfcae7ff
Warren Fauvel <warren.fauvel@gmail.com>
2019-03-02 13:18:49 +0100

    Engine page updates

--- .vuepress/theme/layouts/Engine.vue
+++ .vuepress/theme/layouts/Engine.vue
@@ -18,130 +18,143 @@
             >{{ actionLink.text }}</router-link>
           </p>
         </div>
-        <div class="flex-1">
-          <img src="/rocket.svg" class="w-full"> 
+        <div class="flex-1 w-auto mx-auto md:w-1/2">
+          <img src="/rocket.svg" class="w-full">
         </div>
       </header>
     </div>
 
     <section
-      class="font-sans-serif text-smooth bg-white antialiased leading-normal mx-auto p-4 bg-repeat bg-full"
+      class="font-sans-serif text-smooth bg-white antialiased leading-normal mx-auto p-8 bg-repeat bg-full"
       style="background-image: url('/dot.svg');"
     >
-      <h1 class="font-serif border-b-4 border-green mx-8">Processes:
-        <span class="font-sans text-xl font-light italic text-blue-light">The Cylinder</span>
-      </h1>
+      <div class="p-8">
+        <h1 class="font-serif border-b-4 border-green mx-8">
+          Processes:
+          <span class="font-sans text-xl font-light italic text-blue-light">The Cylinder</span>
+        </h1>
 
-      <div class="flex flex-wrap items-top">
-        <!--col1-->
-        <div class="md:w-1/3 mx-8">
-          <p
-            class="py-4"
-          >Not every engine has the same specification, some are built for speed, some for efficiency, some for longevity. Our model allows your organisation to tailor its approach, based on the resources available and the strategic direction you set.</p>
-          <!--Engine cycles-->
-          <div class="text-xs bg-blue-lightest rounded p-4">
-            <h4 class="text-lg text-green-darker">Our Engine Cycles:</h4>
-            <p class="border-b-2 border-green my-2 py-2">
-              <span class="font-bold text-base">1. Project Inception -</span>working with your team from strategy to operations to define project requirements
-            </p>
-            <p class="border-b-2 border-green my-2 py-2">
-              <span class="font-bold text-base">2. Project Hot-Housing -</span> rapidly developing solutions through our network and your capabilities
-            </p>
-            <p class="border-b-2 border-green my-2 py-2">
-              <span class="font-bold text-base">3. Product Management -</span> expert support in design, infrastructure, development and regulatory awareness e.g. GDPR
-            </p>
-            <p class="border-b-2 border-green my-2 py-2">
-              <span class="font-bold text-base">4. Pilot Go-to-Market -</span> leveraging your market position to validate models and generate digital P&L
-            </p>
-            <p class="border-b-2 border-green my-2 py-2">
-              <span class="font-bold text-base">5. Innovation Accounting -</span> ensuring your processes and KPIs are aligned to measure value return over time
+        <div class="flex flex-wrap items-top">
+          <!--col1-->
+          <div class="md:w-1/3 mx-8">
+            <p
+              class="py-4"
+            >Not every engine has the same specification, some are built for speed, some for efficiency, some for longevity. Our model allows your organisation to tailor its approach, based on the resources available and the strategic direction you set.</p>
+            <!--Engine cycles-->
+            <div class="text-xs bg-blue-lightest rounded p-4">
+              <h4 class="text-lg text-blue-darkest">Engine Cycles:</h4>
+              <p class="border-b-2 border-green my-2 py-2">
+                <span class="font-bold text-base">1. Project Inception -</span>working with your team from strategy to operations to define project requirements
+              </p>
+              <p class="border-b-2 border-green my-2 py-2">
+                <span class="font-bold text-base">2. Project Hot-Housing -</span> rapidly developing solutions through our network and your capabilities
+              </p>
+              <p class="border-b-2 border-green my-2 py-2">
+                <span class="font-bold text-base">3. Product Management -</span> expert support in design, infrastructure, development and regulatory awareness e.g. GDPR
+              </p>
+              <p class="border-b-2 border-green my-2 py-2">
+                <span class="font-bold text-base">4. Pilot Go-to-Market -</span> leveraging your market position to validate models and generate digital P&L
+              </p>
+              <p class="border-b-2 border-green my-2 py-2">
+                <span class="font-bold text-base">5. Innovation Accounting -</span> ensuring your processes and KPIs are aligned to measure value return over time
+              </p>
+            </div>
+            <!--./Engine cycles-->
+            <p>
+              <a
+                href="/projects"
+                class="no-underline font-serif font-semibold text-lg text-blue block bg-white w-64 py-2 px-4 my-4 mb-8 mx-auto text-center shadow-green border-2 border-green hover:bg-green hover:shadow-white hover:text-white"
+              >see our engine running</a>
             </p>
           </div>
-          <!--./Engine cycles-->
+          <!--./col1-->
+          <!--col2-->
+          <div class="p-8 md:flex-1">
+            <img src="/engineprocess.svg" class="w-full mx-auto">
+          </div>
+          <!--./col2-->
         </div>
-        <!--./col1-->
-        <!--col2-->
-        <div class="p-4 md:flex-1">
-          <img src="/engineprocess.svg" class="w-full mx-auto">
-          <div class="m-2 p-2">
-            <p>Some examples of our work:</p>
-            <div class="flex flex-wrap items-top text-xs">
-              <div class="w-full md:w-1/3">
-                <p>Zealous - Designing a self-sustaining GovTech ecosystem for Wales, focused on business creation and social innovation</p>
-              </div>
-              <div class="w-full md:w-1/3">
-                <p>ED-GE - Supporting a Generali Group company to reposition itself to take advantage of Open Innovation Practices and support a 1bn EUR Innovation & Digital Transformation Strategy</p>
-              </div>
-              <div class="w-full md:w-1/3">
-                <p>See more...</p>
-              </div>
+      </div>
+      <div class="p-8">
+        <h1 class="font-serif border-b-4 border-green mx-8">
+          Network:
+          <span class="font-sans text-xl font-light italic text-blue-light">The Fuel</span>
+        </h1>
+        <div class="flex flex-wrap items-top">
+          <!--col1-->
+          <div class="md:w-1/3 mx-8">
+            <p class="py-4">
+              Our portfolio companies bring a unique perspective on how traditional markets are changing and what bricks and mortar businesses can do to leverage their brand and reach to deliver digital P&L through partnerships with our ecosystem companies. We have direct access to a network of over 30 transformative thinkers (and a reach of hundreds more).
+              Take a look at our portfolio/network >
+            </p>
+            <!--Engine cycles-->
+            <div class="text-xs bg-blue-lightest rounded p-4">
+              <h4 class="text-lg text-green-darker">Markets we cover:</h4>
+              <ul class="border-b-2 border-green my-2 py-2">
+                <li>Energy</li>
+                <li>Healthcare</li>
+                <li>Insurance</li>
+                <li>Automotive</li>
+                <li>Crypto & Digital Assets</li>
+                <li>Public Services</li>
+                <li>Advertising</li>
+                <li>Information Technology</li>
+                <li>Telecoms</li>
+                <li>Utilities</li>
+              </ul>
             </div>
+            <p>
+              <a
+                href="/projects"
+                class="no-underline font-serif font-semibold text-lg text-blue block bg-white w-64 py-2 px-4 my-4 mb-8 mx-auto text-center shadow-green border-2 border-green hover:bg-green hover:shadow-white hover:text-white"
+              >take a look at our network</a>
+            </p>
+            <!--./Engine cycles-->
+          </div>
+          <!--./col1-->
+          <!--col2-->
+          <div class="p-4 md:flex-1 md:m-8">
+            <img src="/globalskills.svg" class="w-full mx-auto">
           </div>
         </div>
         <!--./col2-->
       </div>
-      <h1 class="font-serif border-b-4 border-green mx-8">Network:
-        <span class="font-sans text-xl font-light italic text-blue-light">The Fuel</span>
-      </h1>
-      <div class="flex flex-wrap items-top">
-        <!--col1-->
-        <div class="md:w-1/3 mx-8">
-          <p class="py-4">
-            Our portfolio companies bring a unique perspective on how traditional markets are changing and what bricks and mortar businesses can do to leverage their brand and reach to deliver digital P&L through partnerships with our ecosystem companies. We have direct access to a network of over 30 transformative thinkers (and a reach of hundreds more).
-            Take a look at our portfolio/network >
-          </p>
-          <!--Engine cycles-->
-          <div class="text-xs bg-blue-lightest rounded p-4">
-            <h4 class="text-lg text-green-darker">Markets we cover:</h4>
-            <p class="border-b-2 border-green my-2 py-2">
-              Energy
-              Healthcare
-              Insurance
-              Automotive
-              Crypto & Digital Assets
-              Public Services
-              Advertising
-              Information Technology
-              Telecoms
-              Utilities
-            </p>
+
+      <div class="p-8">
+        <h1 class="font-serif border-b-4 border-green mx-8">
+          Platform:
+          <span class="font-sans text-xl font-light italic text-blue-light">The Controls</span>
+        </h1>
+        <div class="flex flex-wrap items-top">
+          <!--col1-->
+          <div class="md:w-1/3 mx-8">
+            <p
+              class="py-4"
+            >Multi-group Stakeholder Management is key to starting and sustaining an innovation engine. We bring tools designed to ease communication, decision making and promotion, allowing to press the right pedal as you travel.</p>
+            <!--Engine cycles-->
+            <div class="text-xs bg-blue-lightest rounded p-4">
+              <h4 class="text-lg text-green-darker">Platform Stakeholders:</h4>
+              <ul>
+                <li>Global Leadership - ensuring strategic alignment and support through clear metrics and evidence-based insights</li>
+                <li>
+                  Internal Teams - platforms to promote buy-in and encourage support and further insights from internal teams</li>
+                  <li>Market Influencers - communicate innovation value throughout project-pipeline
+                </li>
+                <li>External Innovators - engaging and managing your innovation partners to ensure alignment, compliance and delivery</li>
+                <li>Customers - identifying and engaging key customer groups as you begin go-to-market activities</li>
+                <li>Regulators - ensuring well-documented audit and compliance trails exist for each project</li>
+              </ul>
+            </div>
+            <!--./Engine cycles-->
           </div>
-          <!--./Engine cycles-->
-        </div>
-        <!--./col1-->
-        <!--col2-->
-        <div class="p-4 md:flex-1">
-          <img src="/engineprocess.svg" class="w-full mx-auto">
-          <div class="m-2 p-2">
-            <p>
-              Areas of expertise:
-              Machine Learning
-              Distributed networks
-              Software Development
-              Insurance management
-              Internet of Things
-              Data Science
-              Behavioral Psychology
-              Privacy & Data Security
-              Venture Capital
-              Digital Transformation
-            </p>
+          <!--./col1-->
+          <!--col2-->
+          <div class="p-4 md:flex-1 md:m-8">
+            <img src="/enginemetrics.svg" class="w-full mx-auto">
           </div>
         </div>
+        <!--./col2-->
       </div>
-      <!--./col2-->
-      <h1>Platform</h1>Image - stakholder flows
-      <p>Multi-group Stakeholder Management is key to starting and sustaining an innovation engine. We bring tools designed to ease communication, decision making and promotion, allowing to press the right pedal as you travel. Our tools focus on three key groups:</p>
-      <ul>
-        <li>Global Leadership - ensuring strategic alignment and support through clear metrics and evidence-based insights</li>
-        <li>
-          Internal Stakeholders - platforms to promote buy-in and encourage support and further insights from internal teams
-          Market Influencers - communicate innovation value throughout project-pipeline
-        </li>
-        <li>External Innovators - engaging and managing your innovation partners to ensure alignment, compliance and delivery</li>
-        <li>Customers - identifying and engaging key customer groups as you begin go-to-market activities</li>
-        <li>Regulators - ensuring well-documented audit and compliance trails exist for each project</li>
-      </ul>
     </section>
 
     <Content custom/>
